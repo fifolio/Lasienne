5e88e95f73affea8796bcffa7361ee4364b88e1f
Firas <firasdabbabi@gmail.com>
2023-07-13 22:57:22 +0100

    set Footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,43 @@
-export default function Footer(){
+import "./Footer.scss";
+
+export default function Footer() {
     return (
-        <>
-        FOOTER
-        </>
+        <div className="footer">
+            <div className="top">
+                <div className="item">
+                    <h1>Categories</h1>
+                    <span>Women</span>
+                    <span>Men</span>
+                    <span>Shoes</span>
+                    <span>Accessories</span>
+                    <span>New Arrivals</span>
+                </div>
+                <div className="item">
+                    <h1>Links</h1>
+                    <span>FAQ</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cookies</span>
+                </div>
+                <div className="item">
+                    <h1>About</h1>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae omnis dolorem, officia ducimus perferendis laboriosam eum similique dicta, perspiciatis inventore quis aut. Architecto dignissimos illo.
+                </div>
+                <div className="item">
+                    <h1>Contact</h1>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae omnis dolorem, officia ducimus perferendis laboriosam eum similique dicta, perspiciatis inventore quis aut. 
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="left">
+                    <span className="logo">Crystals</span>
+                    <span className="copyright">&copy; {new Date().getFullYear()}. All Rights Reserved</span>
+                </div>
+                <div className="right">
+                    <img src="/img/payment.png" />
+                </div>
+            </div>
+        </div>
     )
 }
