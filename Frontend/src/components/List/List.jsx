357d219640b57787dad0841eb44ed6eb97721845
FIRAS <97522852+fifolio@users.noreply.github.com>
2023-07-21 18:05:14 +0000

    u

--- Frontend/src/components/List/List.jsx
+++ Frontend/src/components/List/List.jsx
@@ -6,13 +6,13 @@ import "./List.scss";
 
 export default function List({ catId, subCats, maxPrice, sort }) {
 
-    const { data, loading, error } = useFetch(`/products?populate=*&[filters][categories][id]=${catId}}
+    const { data, loading, error } = useFetch(`/products?populate=*&[filters][categories][id]=${catId}${subCats.map((item) => `&[filters][sub_categories][id][$eq]=${item}`)}
     `);
 
     // &[filters][categories][id]=${catId}${subCats.map((item) => `&[filters][sub_categories][id][$eq]=${item}`)}
     // &[filters][price][$lte]=${maxPrice}&sort=price:${sort ? sort : 'desc'
 
-    console.log(data)
+    console.log(1, data)
 
 
     return (
