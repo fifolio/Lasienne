faaa147035125d7ceeb02c12f4ae5d36caf72a0b
FIRAS <97522852+fifolio@users.noreply.github.com>
2023-07-21 17:03:31 +0000

    u

--- Frontend/src/components/Card/Card.jsx
+++ Frontend/src/components/Card/Card.jsx
@@ -11,18 +11,15 @@ export default function Card({ item }) {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
-    console.log(item.attributes?.img?.data.attributes.url)
-    console.log('First image URL:', import.meta.env.VITE_BASE_URL + item.attributes?.img?.data.attributes.url);
-    console.log('Second image URL:', import.meta.env.VITE_BASE_URL + item.attributes?.img2?.data.attributes.url);
-    
+  
     return (
 
         <Link className='link' to={`/product/${item?.id}`} onClick={scrollUp}>
             <div className="card">
                 <div className="image">
                     {item?.attributes.isNew && <span>⭐ New Season</span>}
-                    <img src={import.meta.env.VITE_BASE_URL + item.attributes?.img?.data.attributes.url} className='mainImg' />
-                    <img src={import.meta.env.VITE_BASE_URL + item.attributes?.img2?.data.attributes.url} className='secondImg' />
+                    <img src={item.attributes?.img?.data.attributes.url} className='mainImg' />
+                    <img src={item.attributes?.img?.data.attributes.url} className='secondImg' />
                 </div>
                 <h2>{item?.attributes.title}</h2>
                 <div className='prices'>
