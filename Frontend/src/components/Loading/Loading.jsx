07ab0b677e64b957597609678c17ca3687aee6ab
FIRAS <97522852+fifolio@users.noreply.github.com>
2023-11-12 16:50:54 +0000

    add Countdown.jsx

--- Frontend/src/components/Loading/Loading.jsx
+++ Frontend/src/components/Loading/Loading.jsx
@@ -1,10 +1,14 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import Countdown from '../Countdown/Countdown';
 
 export default function CircularIndeterminate() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
-      <CircularProgress />
-    </Box>
+    <>
+      <Box sx={{textAlign: 'center', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+      <Countdown />
+        <CircularProgress />
+      </Box>
+    </>
   );
 }
